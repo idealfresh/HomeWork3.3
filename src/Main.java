882b693d0d117e95fb27d8241f0db093fdf05759
owner90 <owner90@192.168.5.3>
2016-10-06 15:23:51 +0300

    Приема две целочислени и извежда остатъка

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Напишете проргама която приема 2 целочислени числа за вход
+		//и извежда остатъка от делението на числата.
+
+int a=5;
+int b=2;
+double c=a%b;
+System.out.println(c);
+	}
+
+}
